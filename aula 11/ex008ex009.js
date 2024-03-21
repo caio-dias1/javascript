f883b15c8766248b2f6046459d5e1c3d487a0bd7
Caio Dias <caior.dias11@gmail.com>
2024-03-20 22:39:46 -0300

    conditions

---
+++ aula 11/ex008ex009.js
@@ -0,0 +1,15 @@
+var pais = 'EUA'
+console.log(`Seu país de origem é: ${pais}`)
+if (pais == 'Brasil') {
+    console.log('voce é brasileiro!')
+} else {
+    console.log('voce é estrangeiro!')
+}
+
+var vel = 62
+console.log(`A velocidade do seu veiculo é de ${vel} km/h`)
+if (vel > 60) {
+    console.log('Voce foi multado por excesso de velocidade!')
+} else {
+     console.log('Boa viagem e use sempre o cinto de segurança!')
+}
