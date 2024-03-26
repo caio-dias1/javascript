2562226900fc4f02e1e6e5a4641bb23b8c473322
Caio Dias <caior.dias11@gmail.com>
2024-03-26 20:22:43 -0300

    ex horas

--- aula12ex/ex014/script.js
+++ aula12ex/ex014/script.js
@@ -7,11 +7,17 @@ function carregar() {
     if (hora >= 0 && hora < 12) {
         // BOM DIA
         img.src = 'imagens/manha.png'
+        document.body.style.background = '#9e9e05'
     } else if (hora > 12 && hora < 19) {
         // BOA TARDE
         img.src = 'imagens/tarde.png'
+        document.body.style.background = '#ca5327'
+    } else if (hora > 24) {
+        window.alert('HORARIO INVALIDO!')
     } else {
         // BOA NOITE
         img.src = 'imagens/noite.png'
+        document.body.style.background = '#1c1c7e'
     }
+    
 }
