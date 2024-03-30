5b86f50abc6864d45ed8391728ffb28e3b852e0c
Caio Dias <caior.dias11@gmail.com>
2024-03-30 14:55:17 -0300

    function calcular idade

--- aula12ex/ex015/script.js
+++ aula12ex/ex015/script.js
@@ -1,3 +1,10 @@
 function verificar () {
-
+    let nasc = document.getElementById('ano')
+    let ano = Number(nasc.value)
+    let texto = document.getElementById('texto')
+    let img = document.getElementById('foto')
+    idade = 2024 - ano
+    texto.innerHTML = `Voce tem ${idade} anos`
+    
+    
 }
