85587e6273feabcbc8a4a79060bff39f6863dc1a
Caio Dias <caior.dias11@gmail.com>
2024-04-12 23:12:28 -0300

    do-while-for

---
+++ aula 13/ambiente.js
@@ -0,0 +1,14 @@
+// estrutura de repeticao com teste logico no inicio
+
+let c = 1
+while (c <= 6) {
+    console.log(`Passo ${c}`)
+    c++
+} 
+
+// estrutura de repeticao com teste logico no final
+
+do {
+    console.log(`Passo ${c}`)
+    c++
+} while (c <= 4)
