2c1744b49109520d9df5c1ab432b0f66a94d43f9
Caio Dias <caior.dias11@gmail.com>
2024-04-16 22:28:24 -0300

    fim-exe-contagem

--- aula 14ex/ex016/script.js
+++ aula 14ex/ex016/script.js
@@ -3,13 +3,30 @@ function contar () {
     let fim = document.querySelector('input#fim')
     let passo = document.querySelector('input#passo')
     let resultado = document.querySelector('p#resultado')
-    let n1 = inicio.value
-    let n2 = fim.value
-    let n3 = passo.value
-        while (n1 <= n2) {
-            resultado.innerHTML = `Contando:<br> ${inicio}`
-            n1 = n1 + n3
+    if (inicio.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
+        resultado.innerHTML = 'Impossivel contar!'
+    } else {
+        resultado.innerHTML = 'Contando: <br>'
+        let i = Number(inicio.value)
+        let f = Number(fim.value)
+        let p = Number(passo.value)
+        if (p <= 0) {
+            window.alert('Passo invalido! Considerando Passo 1')
+            p = 1
         }
-    
-    
+        if (i < f) {
+            // contagem crescente
+            for(let c = i; c <= f; c += p) {
+                resultado.innerHTML += `${c} &#x1F449 `
+                } 
+        } else {
+            // contagem decrescente
+            for(let c = i; c >= f; c -= p) {
+                resultado.innerHTML += `${c} &#x1F449 `
+            }
+        }
+        resultado.innerHTML += `\u{1F3C1}`
+        
+    }
 }
+    
