e2f759794bd498972dd1337762e87e185c989319
Caio Dias <caior.dias11@gmail.com>
2024-04-18 22:19:04 -0300

    versao-funcional
    versao final multiplicando o numero escolhido ate 10. exercicio resolvido sozinho

--- aula 14ex/ex017/script.js
+++ aula 14ex/ex017/script.js
@@ -1,3 +1,19 @@
 function gerar () {
-
+    let num = document.querySelector('input#num')
+    let res = document.querySelector('p#res')
+    if (num.value.length == 0) {
+        window.alert('Digite um numero!')
+    } else {
+        let n = Number(num.value)
+        let t = 0
+        res.innerHTML = 'Multiplicando!<br>'
+        while (t <= 10) {
+            let r = n * t
+            res.innerHTML += `${n} x ${t} = ${r} <br>`
+            t ++ 
+        }
+        
+    }
+    
+    
 }
