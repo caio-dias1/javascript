80b5428be05057a7f73cad80e3b5a928b7124365
Caio Dias <caior.dias11@gmail.com>
2024-06-14 20:38:32 -0300

    testando array

---
+++ aula 15/ambiente2.js
@@ -0,0 +1,10 @@
+let valores = [ ]
+function inserir(n) {
+    valores.push(n)
+    console.log(`Numero ${n} adicionado a lista`)
+    console.log(`Lista atual: ${valores}`)
+}
+console.log(inserir(6))
+console.log(inserir(7))
+console.log(inserir(9))
+
