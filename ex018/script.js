4f771cd3a64fdcb00972199b54cab4fe30178db4
Caio Dias <caior.dias11@gmail.com>
2024-06-22 20:15:23 -0300

    finish

--- ex018/script.js
+++ ex018/script.js
@@ -21,10 +21,41 @@ function inLista(n, l) {
 
 function add() {
     if(isNumero(num.value) && !inLista(num.value, valores)) {
-        valores.push(number)
-        
+        valores.push(Number(num.value))
+        let item = document.createElement('option')
+        item.text = `Valor ${num.value} adicionado.`
+        lista.appendChild(item)
+        res.innerHTML = ''
     } else {
         window.alert('Numero invalido ou já encontrado na lista.')
+    }
+    num.value = ''
+    num.focus()
+}
+
+function finalizar() {
+    if (valores.length == 0) {
+        window.alert('Adicione um número.')
+    } else {
+        let total = valores.length
+        let maior = valores[0]
+        let menor = valores[0]
+        let soma = 0
+        let media = 0
+        for(let pos in valores) {
+            soma += valores[pos]
+            media = soma / total
+            if (valores[pos] > maior)
+                maior = valores[pos]
+            if (valores[pos] < menor)
+                menor = valores[pos]
+        }
+        res.innerHTML = ''
+        res.innerHTML += `<p> No momento possui ${total} valores cadastrados. </p>`
+        res.innerHTML += `<p>O maior valor informado foi ${maior}.</p>`
+        res.innerHTML += `<p>O menor valor informado foi ${menor}.</p>`
+        res.innerHTML += `<p>A soma total dos valores foi de ${soma}.</p>`
+        res.innerHTML += `<p>A media dos valores foi de ${media}.</p>`
     }
 }
 
